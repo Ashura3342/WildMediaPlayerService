20edf15ab907ca5215255bb83429e5e48a2faaee
sandrine <poupardsandrine@gmail.com>
2018-12-20 11:16:00 +0100

    Layout and switch on Button Listener done.

--- app/src/main/java/fr/wildcodeschool/wildmediaplayerservice/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/wildmediaplayerservice/MainActivity.java
@@ -2,12 +2,47 @@ package fr.wildcodeschool.wildmediaplayerservice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button playButton =  findViewById(R.id.button_play);
+        Button pauseButton =  findViewById(R.id.button_pause);
+        Button stopButton =  findViewById(R.id.button_stop);
+        playButton.setOnClickListener(this);
+        pauseButton.setOnClickListener(this);
+        stopButton.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        int id= v.getId();
+
+        switch (id){
+            case R.id.button_play : onClickPlay(); break;
+            case R.id.button_pause : onClickPause(); break;
+            case R.id.button_stop : onClickStop(); break;
+            default: throw new IllegalArgumentException(String.format(getString(R.string.bad_id_exception_text) , id) );
+        }
+    }
+
+    private void onClickPlay() {
+    }
+
+    protected void  onClickPause(){
+
+    }
+
+    private void onClickStop() {
+    }
+
+
+
+
 }
